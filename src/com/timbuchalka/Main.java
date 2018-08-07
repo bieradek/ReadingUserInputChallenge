b2b266661a7b26b306d01e4aed8ffeaf50da729d
Radoslaw Biedrzycki <radoslaw.biedrzycki@syncron.com>
2018-08-07 18:27:47 +0200

    Uploading Tim's code

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -4,36 +4,52 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
+    //    public static void main(String[] args) {
+//
 //        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please provide input");
-//        boolean hasNextInt = scanner.hasNextInt();
-//        System.out.println(hasNextInt);
-
-
+//        int counter = 1;
+//        int sum = 0;
+//        while (counter <= 10) {
+//            System.out.println("Enter number #" + counter + ":");
+//            boolean hasNextInt = scanner.hasNextInt();
+//            if (hasNextInt) {
+//                int input = scanner.nextInt();
+//                sum += input;
+//                counter++;
+//            } else {
+//                scanner.nextLine();
+//                System.out.println("Invalid Number");
+//                scanner.nextLine();
+//            }
+//        }
+//        System.out.println(sum);
+//        scanner.close();
+//    }
+    /*
+    TIMS CODE
+     */
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int counter = 1;
+        int counter = 0;
         int sum = 0;
-        while (counter <= 10) {
-            System.out.println("Enter number #" + counter + ":");
-            boolean hasNextInt = scanner.hasNextInt();
-            if (hasNextInt)
-            {
-                int input = scanner.nextInt();
-                //scanner.nextLine(); not necessary?
-                sum += input;
+
+        while (true) {
+            int order = counter + 1;
+            System.out.println("Enter number %" + order + ":");
+            boolean isAnInt = scanner.hasNextInt();
+            if (isAnInt) {
+                int number = scanner.nextInt();
                 counter++;
-            }
-
-            else
-
-                {
+                sum += number;
+                if (counter == 10) {
+                    break;
+                }
+            } else {
                 System.out.println("Invalid Number");
             }
+            scanner.nextLine(); // handle end of line (enter key)
         }
-        System.out.println(sum);
+        System.out.println("Sum equals =" + sum);
         scanner.close();
-
     }
 }
